788f65e71cdd2fdc23565ec5eef925500e026d28
Mallen220 <allenmc220@gmail.com>
2025-08-18 10:24:25 +0200

    Update add-to-cart logic and BuyMeACoffee widget
    Refactored add-to-cart button selector and added event listeners for multiple buttons in add-to-cart.js. Updated BuyMeACoffee widget configuration in head.html for dynamic positioning and changed data-id. Included add-to-cart.js in product and shop layouts. Removed cart markup from cart.html and set layout to default.

--- assets/js/add-to-cart.js
+++ assets/js/add-to-cart.js
@@ -1,4 +1,4 @@
-jQuery("#item_add").click(function () {
+jQuery("#add-to-cart").click(function () {
   $("html, body").animate(
     {
       scrollTop: 0,
@@ -7,4 +7,19 @@ jQuery("#item_add").click(function () {
   );
 
   $("#cart-menu").addClass("animated shake");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".add-to-cart").forEach((button) => {
+    button.addEventListener("click", function () {
+      simpleCart.add({
+        name: this.dataset.name,
+        price: this.dataset.price,
+        number: this.dataset.id,
+        image: this.dataset.image,
+        type: this.dataset.type,
+        subscription: this.dataset.subscription,
+      });
+    });
+  });
 });
